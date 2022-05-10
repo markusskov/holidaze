d3d280a9a52b14b09ea63cce802135ed2e3ee8d3
Markus Skov <69841783+markusskov@users.noreply.github.com>
2022-05-10 19:03:13 +0200

    Styling to add hotel form

--- pages/dashboard/addHotel/index.js
+++ pages/dashboard/addHotel/index.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import styles from './Addhotel.module.scss';
 import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-import { fetcher } from '../../../lib/api';
 
 const AddHotel = ({}) => {
   // Check if user is logged in
@@ -17,12 +16,13 @@ const AddHotel = ({}) => {
   const [hotelName, sethotelName] = useState('');
   const [hotelDesc, sethotelDesc] = useState('');
   const [hotelFullDesc, sethotelFullDesc] = useState('');
-  const [hotelPrice, sethotelPrice] = useState(10);
+  const [hotelPrice, sethotelPrice] = useState('');
   const [hotelCountry, sethotelCountry] = useState('');
   const [hotelCity, sethotelCity] = useState('');
   const [hotelAllInclusive, sethotelAllInclusive] = useState(false);
   const [hotelImg, sethotelImg] = useState('');
 
+  // Sending info to Strapi
   async function NewHotel() {
     const jwt = Cookies.get('jwt');
 
@@ -62,64 +62,76 @@ const AddHotel = ({}) => {
         {isLogged ? (
           <>
             <p className={styles.welcome}>
-              👋🏼 &nbsp;Welcome back, <b>{Cookies.get('username')}</b>!
+              Fill out the form and click add new hotel to publish.
               <br />
-              Edit hotels, or add new ones.
             </p>
-            <form>
-              <input
-                type="text"
-                onChange={(e) => sethotelName(e.target.value)}
-                value={hotelName}
-                placeholder="Hotel Name"
-              />
-              <br />
-              <input
-                type="text"
-                onChange={(e) => sethotelDesc(e.target.value)}
-                value={hotelDesc}
-                placeholder="Hotel Short Description"
-              />
-              <br />
-              <input
-                type="text"
-                onChange={(e) => sethotelFullDesc(e.target.value)}
-                value={hotelFullDesc}
-                placeholder="Hotel Full description"
-              />
-              <br />
-              <input
-                type="number"
-                onChange={(e) => sethotelPrice(e.target.value)}
-                value={hotelPrice}
-                placeholder="Hotel Price"
-              />
-              <br />
-              <input
-                type="text"
-                onChange={(e) => sethotelCountry(e.target.value)}
-                value={hotelCountry}
-                placeholder="Hotel Country"
-              />
-              <br />
-              <input
-                type="text"
-                onChange={(e) => sethotelCity(e.target.value)}
-                value={hotelCity}
-                placeholder="Hotel City"
-              />
-              <br />
-              <input
-                type="text"
-                onChange={(e) => sethotelImg(e.target.value)}
-                value={hotelImg}
-                placeholder="Hotel Image Url"
-              />
-              <br />
-              <button type="button" onClick={NewHotel}>
-                Add new hotel
-              </button>
-            </form>
+            <div className={styles.formContainer}>
+              <form className={styles.form}>
+                <input
+                  className={styles.formInput}
+                  type="text"
+                  onChange={(e) => sethotelName(e.target.value)}
+                  value={hotelName}
+                  placeholder="Hotel Name"
+                />
+                <br />
+                <input
+                  className={styles.formInput}
+                  type="text"
+                  onChange={(e) => sethotelDesc(e.target.value)}
+                  value={hotelDesc}
+                  placeholder="Hotel Short Description"
+                />
+                <br />
+                <textarea
+                  className={styles.formInput}
+                  type="text"
+                  onChange={(e) => sethotelFullDesc(e.target.value)}
+                  value={hotelFullDesc}
+                  placeholder="Hotel Full description"
+                />
+                <br />
+                <input
+                  className={styles.formInput}
+                  type="number"
+                  onChange={(e) => sethotelPrice(e.target.value)}
+                  value={hotelPrice}
+                  placeholder="Hotel Price"
+                />
+                <br />
+                <input
+                  className={styles.formInput}
+                  type="text"
+                  onChange={(e) => sethotelCountry(e.target.value)}
+                  value={hotelCountry}
+                  placeholder="Hotel Country"
+                />
+                <br />
+                <input
+                  className={styles.formInput}
+                  type="text"
+                  onChange={(e) => sethotelCity(e.target.value)}
+                  value={hotelCity}
+                  placeholder="Hotel City"
+                />
+                <br />
+                <input
+                  className={styles.formInput}
+                  type="text"
+                  onChange={(e) => sethotelImg(e.target.value)}
+                  value={hotelImg}
+                  placeholder="Hotel Image Url"
+                />
+                <br />
+                <button
+                  className={styles.formButton}
+                  type="button"
+                  onClick={NewHotel}
+                >
+                  Add new hotel
+                </button>
+              </form>
+            </div>
           </>
         ) : (
           <>
